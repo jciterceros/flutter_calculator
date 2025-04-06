f36d9d4ac2b2a4f68c7aff7ea9224948f994f23a
jciterceros <jciterceros@yahoo.com>
2025-04-06 13:20:52 -0300

    refactor: improve Display component readability and structure

--- lib/components/display.dart
+++ lib/components/display.dart
@@ -6,31 +6,40 @@ class Display extends StatelessWidget {
 
   const Display({super.key, this.displayText = "0"});
 
+  static const _backgroundColor = Color.fromRGBO(48, 48, 48, 1);
+  static const _padding = EdgeInsets.all(8.0);
+  static const double _minFontSize = 20.0;
+  static const double _maxFontSize = 80.0;
+  static const int _maxLines = 1;
+
+  static TextStyle _getTextStyle() {
+    return const TextStyle(
+      fontWeight: FontWeight.w100,
+      decoration: TextDecoration.none,
+      fontSize: 80,
+      color: Colors.white,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      flex: 1,
       child: Container(
-        color: Color.fromRGBO(48, 48, 48, 1),
+        color: _backgroundColor,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: (AutoSizeText(
-                displayText,
-                minFontSize: 20,
-                maxFontSize: 80,
-                maxLines: 1,
+              padding: _padding,
+              child: AutoSizeText(
+                displayText.isNotEmpty ? displayText : "0",
+                minFontSize: _minFontSize,
+                maxFontSize: _maxFontSize,
+                maxLines: _maxLines,
                 textAlign: TextAlign.end,
-                style: TextStyle(
-                  fontWeight: FontWeight.w100,
-                  decoration: TextDecoration.none,
-                  fontSize: 80,
-                  color: Colors.white,
-                ),
-              )),
+                style: _getTextStyle(),
+              ),
             ),
           ],
         ),

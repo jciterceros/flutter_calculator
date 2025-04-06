2286ba189c6c59315d4ecef14c1129f0941d4d98
jciterceros <jciterceros@yahoo.com>
2025-04-06 11:02:58 -0300

    refactor: improve Button component readability and structure

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
-  static const DARK = Color.fromRGBO(82, 82, 82, 1);
-  static const DEFAULT = Color.fromRGBO(112, 112, 112, 1);
-  static const OPERATORS = Color.fromRGBO(250, 158, 13, 1);
+  static const darkGray = Color.fromRGBO(82, 82, 82, 1);
+  static const defaultGray = Color.fromRGBO(112, 112, 112, 1);
+  static const orangeOperator = Color.fromRGBO(250, 158, 13, 1);
+
   final String buttonText;
   final bool bigButton;
   final Color color;
-
-  final void Function(String)? cb;
+  final void Function(String) cb;
 
   const Button({
     super.key,
     required this.buttonText,
     this.bigButton = false,
-    this.color = DEFAULT,
-    @required this.cb,
+    this.color = defaultGray,
+    required this.cb,
   });
+
+  static const EdgeInsets buttonPadding = EdgeInsets.all(0);
+  static const TextStyle buttonTextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 32,
+    fontWeight: FontWeight.w200,
+    decoration: TextDecoration.none,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -26,22 +34,10 @@ class Button extends StatelessWidget {
         style: ElevatedButton.styleFrom(
           backgroundColor: color,
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
-          padding: EdgeInsets.all(0),
+          padding: buttonPadding,
         ),
-        onPressed: () {
-          if (cb != null) {
-            cb!(buttonText);
-          }
-        },
-        child: Text(
-          buttonText,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 32,
-            fontWeight: FontWeight.w200,
-            decoration: TextDecoration.none,
-          ),
-        ),
+        onPressed: () => cb(buttonText),
+        child: Text(buttonText, style: buttonTextStyle),
       ),
     );
   }
